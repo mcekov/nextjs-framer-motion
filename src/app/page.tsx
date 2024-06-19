2f4ebe9af73e0979c27c7f1a2a254854871b6ae6
mcekov <mtsekov@zohomail.eu>
2024-06-19 23:17:40 +0300

    Add constants helper file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,50 +5,9 @@ import { animate, useMotionValue, motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import useMeasure from "react-use-measure";
 
-export default function Home() {
-  const images = [
-    {
-      id: "qwe12341234123",
-      imgSrc: "/img/image-1.jpg",
-      alt: "Image 1",
-    },
-    {
-      id: "qwe134454553423",
-      imgSrc: "/img/image-2.jpg",
-      alt: "Image 2",
-    },
-    {
-      id: "qwe123345345345789123",
-      imgSrc: "/img/image-3.jpg",
-      alt: "Image 3",
-    },
-    {
-      id: "qwe12334rwer5345345789123",
-      imgSrc: "/img/image-4.jpg",
-      alt: "Image 4",
-    },
-    {
-      id: "qwe12334534534345345tdfgd5789123",
-      imgSrc: "/img/image-5.jpg",
-      alt: "Image 5",
-    },
-    {
-      id: "qwe123345345345789345123",
-      imgSrc: "/img/image-6.jpg",
-      alt: "Image 6",
-    },
-    {
-      id: "qwe12334rwer5345345787545679123",
-      imgSrc: "/img/image-7.jpg",
-      alt: "Image 7",
-    },
-    {
-      id: "qwe12334534534345345tdfgd57896643677123",
-      imgSrc: "/img/image-8.jpg",
-      alt: "Image 8",
-    },
-  ];
+import { images } from "@/constants";
 
+export default function Home() {
   const FAST_DURATION = 25;
   const SLOW_DURATION = 75;
 
---
+++ src/constants.ts
@@ -0,0 +1,42 @@
+export const images = [
+  {
+    id: "qwe12341234123",
+    imgSrc: "/img/image-1.jpg",
+    alt: "Image 1",
+  },
+  {
+    id: "qwe134454553423",
+    imgSrc: "/img/image-2.jpg",
+    alt: "Image 2",
+  },
+  {
+    id: "qwe123345345345789123",
+    imgSrc: "/img/image-3.jpg",
+    alt: "Image 3",
+  },
+  {
+    id: "qwe12334rwer5345345789123",
+    imgSrc: "/img/image-4.jpg",
+    alt: "Image 4",
+  },
+  {
+    id: "qwe12334534534345345tdfgd5789123",
+    imgSrc: "/img/image-5.jpg",
+    alt: "Image 5",
+  },
+  {
+    id: "qwe123345345345789345123",
+    imgSrc: "/img/image-6.jpg",
+    alt: "Image 6",
+  },
+  {
+    id: "qwe12334rwer5345345787545679123",
+    imgSrc: "/img/image-7.jpg",
+    alt: "Image 7",
+  },
+  {
+    id: "qwe12334534534345345tdfgd57896643677123",
+    imgSrc: "/img/image-8.jpg",
+    alt: "Image 8",
+  },
+];
